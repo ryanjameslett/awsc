759a804d528a9b0d7f33d30ae34665eef5c29e93
Ryan Lett <ryan.james.lett@gmail.com>
2019-09-25 11:10:30 -0700

    Revert to finding packages automatically again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='awsc',
     version='0.2',
-    packages=['awsc', 'awsc.cmds'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'Click',
